2633140756c7781b31e14c191be17b57e505c530
okg001 <okg001@user-no-MacBook-Air.local>
2020-11-11 17:46:10 +0900

    Version chage(16.14.0) add Material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const customer = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : 'cccc',
+    'birthday' : '99 99 99',
+    'gender' : '男',
+    'job' : 'programer'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : 'dddd',
+    'birthday' : '88 88 99',
+    'gender' : '女',
+    'job' : 'programer'
+  },
+  {
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : 'aaaa',
+    'birthday' : '77 99 99',
+    'gender' : '男',
+    'job' : 'programer'
+  }
+]
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        {
+          customer.map(c => {
+            return (
+              <Customer
+                key = {c.id}
+                id = {c.id}
+                image = {c.image}
+                name = {c.name}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
